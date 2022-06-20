fdf1b44d0fc580604b326ff1acd00e44be180b12
MikeyBadi <mihaibadia.1999@gmail.com>
2022-06-20 22:31:45 +0200

    first

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+
+require_once  __DIR__ . '/comp/head';
+require_once  __DIR__ . '/comp/header';
+require_once  __DIR__ . '/comp/main';
+require_once  __DIR__ . '/comp/footer';
+  
